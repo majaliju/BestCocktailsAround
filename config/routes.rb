69da9293e271fd1d652d4a8baea6babe2b6025bd
majaliju <majalijunyc@gmail.com>
2023-04-03 19:01:32 -0400

    models running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :reviews
+  resources :bar_cocktails
+  resources :cocktails
+  resources :bars
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/hello', to: 'application#hello_world'
---
+++ db/migrate/20230403225259_create_bars.rb
@@ -0,0 +1,15 @@
+class CreateBars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bars do |t|
+      t.string :name
+      t.string :city
+      t.string :state
+      t.string :address
+      t.string :latitude
+      t.string :decimal
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
