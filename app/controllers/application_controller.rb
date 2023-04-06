47544e2a40b03356d103e3a8f9025fddfcf40a59
majaliju <majalijunyc@gmail.com>
2023-04-06 17:41:25 -0400

    routes comments config; adding notes to app_controller for method writing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  ##! here is where I'll contain the user authorization pre-checks
+
+
+  ##! this model is for finding the user's location via IP or location allowed
+  def location_finder
+    ## writing the method out
+  end
+
 
   def hello_world
     session[:count] = (session[:count] || 0) + 1
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  resources :bars
-  resources :bar_cocktails
-  resources :cocktails
-  resources :users
-  resources :reviews
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  ####! the 5 below are just only: as a sketch
+  ####! these are not final, these are just during testing purposes
+  ####! there will be likely less for all non-reviews models
+  resources :bars, only: [:index, :show, :create]
+  resources :bar_cocktails, only: [:index, :show, :create]
+  resources :cocktails, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create]
+  resources :reviews, only: [:index, :create, :update, :destroy]
+
+  ## the login, logout, and user-authentication routes
+  get '/me', to: "users#show"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 
   get '/hello', to: 'application#hello_world'
+  get '/location', to: 'application#location_finder'
 
   # Defines the root path route ("/")
   # root "articles#index"
