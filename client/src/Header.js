9f4e3950be72e765cdf54f8e32820d123bead32e
majaliju <majalijunyc@gmail.com>
2023-04-13 18:13:36 -0400

    frontend app runs properly

--- client/src/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div className='navbar bg-primary text-primary-content'>
-      <h1 className='text-xl normal-case btn btn-ghost'>COCKTAILS</h1>
-    </div>
-  );
-}
-
-export default Header;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Homepage from './Homepage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch('/hello')
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
-
   useEffect(() => {
     fetch('/me').then((response) => {
       if (response.ok) {
@@ -29,12 +21,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Homepage />
-      {/* <Router>
-        <Homepage />
-      </Router> */}
-      <h1 className='text-5xl text-cyan-600'>Page Count: {count}</h1>
-      <h1 className='text-3xl font-bold bg-cyan-400'>Hello world!</h1>
+      <Routes>
+        <Route element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
