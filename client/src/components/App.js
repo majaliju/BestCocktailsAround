f8098ee625c994174ac65c614263cb036df00830
majaliju <majalijunyc@gmail.com>
2023-04-13 18:18:54 -0400

    header up and running

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,23 +4,37 @@ import Header from './Header';
 import Homepage from './Homepage';
 
 function App() {
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
   useEffect(() => {
     fetch('/me').then((response) => {
       if (response.ok) {
         response.json().then((user) => {
           console.log('within /me, the response is: ', user);
-          // onLogin(user);
+          onLogin(user);
         });
       } else {
-        console.log('the response failed');
-        // onLogout();
+        onLogout();
       }
     });
   }, []);
 
+  //^ both for logging user in & updating values of LoggedIn & currentUser
+  function onLogin(user) {
+    setUser(user);
+    setLoggedIn(true);
+  }
+
+  //^ to log the user out & also & updating values of LoggedIn & currentUser
+  function onLogout() {
+    setUser({});
+    setLoggedIn(false);
+  }
+
   return (
     <div>
-      <Header />
+      <Header user={user} loggedIn={loggedIn} />
       <Routes>
         <Route element={<Homepage />} />
       </Routes>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@
 import { NavLink, Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
-function Header({ loggedIn, currentUser, onLogout }) {
+function Header({ loggedIn, user, onLogout }) {
   function handleLogout() {
     fetch(`/logout`, {
       method: 'DELETE',
@@ -12,7 +12,7 @@ function Header({ loggedIn, currentUser, onLogout }) {
 
   return (
     <div>
-      <div className='bg-zinc-900 navbar text-primary-content'>
+      <div className='bg-primary navbar text-primary-content'>
         <div className='navbar-start'>
           <div className='dropdown'>
             <label tabIndex={0} className='btn btn-ghost sm:hidden'>
@@ -34,27 +34,15 @@ function Header({ loggedIn, currentUser, onLogout }) {
               tabIndex={0}
               className='w-56 p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box'>
               <li>
-                <NavLink to='/artists' className='font-bold uppercase'>
-                  artists
+                <NavLink to='/' className='font-bold uppercase'>
+                  the best
                 </NavLink>
               </li>
-              <li>
+              {/* <li>
                 <NavLink to='/concerts' className='font-bold uppercase'>
                   concerts
                 </NavLink>
-              </li>
-
-              {/* 
-              {currentUser !== (null || '') ? (
-                <li>
-                  <NavLink
-                    to='/thisUser'
-                    state={{ thisUser: currentUser }}
-                    className='font-bold uppercase'>
-                    User's Page
-                  </NavLink>
-                </li>
-              ) : null} */}
+              </li> */}
 
               <div>
                 {/* {currentUser === (null || '') && (
@@ -84,7 +72,7 @@ function Header({ loggedIn, currentUser, onLogout }) {
             <div className='flex-auto p-4'>
               <Link to='/'>
                 <h3 className='text-5xl normal-case border-none btn btn-secondary btn-outline'>
-                  ticketblaster
+                  COCKTAILS?
                 </h3>
               </Link>
             </div>
@@ -96,19 +84,26 @@ function Header({ loggedIn, currentUser, onLogout }) {
             <ul className='p-0 menu menu-horizontal'>
               <li>
                 <NavLink
-                  to='/artists'
+                  to='/best'
                   className='font-bold uppercase border-none btn btn-outline'>
-                  <h3 className='font-bold uppercase'>artists</h3>
+                  <h3 className='font-bold uppercase'>where's the best?</h3>
                 </NavLink>
               </li>
               <li>
                 <NavLink
-                  to='/concerts'
+                  to='/bars'
                   className='font-bold uppercase border-none btn btn-outline '>
-                  <h3 className='font-bold uppercase'>concerts</h3>
+                  <h3 className='font-bold uppercase'>all bars</h3>
                 </NavLink>
               </li>
-              {loggedIn === true && (
+              <li>
+                <NavLink
+                  to='/cocktails'
+                  className='font-bold uppercase border-none btn btn-outline '>
+                  <h3 className='font-bold uppercase'>all cocktails</h3>
+                </NavLink>
+              </li>
+              {/* {loggedIn === true && (
                 <li>
                   <NavLink
                     to='/thisUser'
@@ -117,7 +112,7 @@ function Header({ loggedIn, currentUser, onLogout }) {
                     Your Activity
                   </NavLink>
                 </li>
-              )}
+              )} */}
             </ul>
           </div>
         </div>
@@ -139,7 +134,7 @@ function Header({ loggedIn, currentUser, onLogout }) {
                     <button
                       className='font-bold uppercase border-none btn btn-outline'
                       onClick={handleLogout}>
-                      logout {currentUser.username}
+                      logout {user.username}
                     </button>
                   </li>
                 </div>
