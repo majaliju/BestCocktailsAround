3357c327928a8e1dda3053cf474d5953ccec3488
majaliju <majalijunyc@gmail.com>
2023-04-13 19:26:13 -0400

    another test

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :latitude, :longitude, :address
+  attributes :id, :username, :password_digest, :latitude, :longitude, :address, :ip_address
 
   has_many :reviews, serializer: ReviewSerializer
 end
