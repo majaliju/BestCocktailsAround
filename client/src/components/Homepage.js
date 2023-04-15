645258170d65e6ec13cde2f527eb2da9b70075c7
majaliju <majalijunyc@gmail.com>
2023-04-14 22:44:21 -0400

    create method refined

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,6 +2,7 @@ function Homepage({ user }) {
   return (
     <div>
       <h1 className='text-5xl text-secondary'>Welcome back {user.username}!</h1>
+      <h1 className='text-5xl text-primary'>search bar & mapkick here</h1>
     </div>
   );
 }
