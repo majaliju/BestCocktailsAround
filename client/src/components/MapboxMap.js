e4cd8420eaee6046a5a24f8d8da99d8cc7fa45fb
majaliju <majalijunyc@gmail.com>
2023-04-15 20:19:35 -0400

    map is on the page

--- client/src/components/MapboxMap.js
+++ client/src/components/MapboxMap.js
@@ -1,5 +1,30 @@
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useState, useRef, useEffect } from 'react';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWFqYWxpanUiLCJhIjoiY2xmaDd3Zjk2MDIzaDNxcnhkZWlreG01eCJ9.Rm4z9DrJF16ukrO8qyWIbQ';
+
 function MapboxMap() {
-  return <div>Testing Mapbox Map</div>;
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  }, []);
+
+  return (
+    <div>
+      <div ref={mapContainer} className='map-container' />
+    </div>
+  );
 }
 
 export default MapboxMap;
