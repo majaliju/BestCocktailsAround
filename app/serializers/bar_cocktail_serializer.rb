85bceea550c8f69b991e0f0358d9877d38c12d9b
majaliju <majalijunyc@gmail.com>
2023-04-26 19:42:17 -0400

    updates on all serializers

--- app/serializers/bar_cocktail_serializer.rb
+++ app/serializers/bar_cocktail_serializer.rb
@@ -1,10 +1,10 @@
 class BarCocktailSerializer < ActiveModel::Serializer
   attributes :id, :cocktail_id, :bar_id, :special_name
 
-  belongs_to :bar
-  belongs_to :cocktail
-  has_many :reviews
-  has_many :users, through: :reviews
+  belongs_to :bar, serializer: BarSerializer
+  belongs_to :cocktail, serializer: CocktailSerializer
+  has_many :reviews, serializer: ReviewSerializer
+  has_many :users, through: :reviews, serializer: UserSerializer
 
 
 end
