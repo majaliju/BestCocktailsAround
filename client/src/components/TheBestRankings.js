6df9ce1e93df24224683e412bc25decd35077c2c
majaliju <majalijunyc@gmail.com>
2023-04-26 17:34:50 -0400

    updating seed data

---
+++ client/src/components/TheBestRankings.js
@@ -0,0 +1,10 @@
+function TheBestRankings() {
+  return (
+    <div>
+      <h2>the top ranked cocktails overall</h2>
+      <h2>sorted by cocktail.reviews(sort by average review.star)</h2>
+    </div>
+  );
+}
+
+export default TheBestRankings;
