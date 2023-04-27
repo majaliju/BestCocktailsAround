d9c203fd96203f79acdb8968cb54a93985d6a586
majaliju <majalijunyc@gmail.com>
2023-04-27 18:47:36 -0400

    notes on ECP, for rendering the page

--- client/src/components/EachBarCocktailPage.js
+++ client/src/components/EachBarCocktailPage.js
@@ -1,6 +1,16 @@
+import { useLocation } from 'react-router-dom';
+
 function EachCocktailPage() {
+  const location = useLocation();
+  let barCocktail = location.state.barCocktail;
+
+  const barCocktailReviews = barCocktail.reviews;
+
+  console.log('barCocktailReviews: ', barCocktailReviews);
+
   // render each barCocktail
-  // render the review of each barCocktail
+  // render the review of each barCocktail in the reviews section
+  //! if barCocktailReviews.length === 0; render <h1>No reviews yet! Feel free to leave one</h1>
   // allow posting a review on any barCocktail
   // do a check if this review matches the user.id --> if so, allow editing and deleting
   return <div></div>;
