27f513ddc6ce1075b3937de1dd64701a82221ea9
majaliju <majalijunyc@gmail.com>
2023-04-27 17:35:53 -0400

    route issue solved

--- client/src/components/EachBarPage.js
+++ client/src/components/EachBarPage.js
@@ -1,4 +1,5 @@
 import { useParams, useLocation } from 'react-router-dom';
+import Loading from './Loading';
 
 function EachBarPage({}) {
   const { id } = useParams();
@@ -8,11 +9,37 @@ function EachBarPage({}) {
   console.log('bar within EACHBARPAGE: ', bar);
   console.log('id within EBC: ', id);
 
-  // set contet here then find the specific bar
   return (
     <div>
-      <h1>{bar.name}</h1>
-      <h1>hey this works</h1>
+      <div className='py-6 bg-base-900 sm:py-8 lg:py-'>
+        {bar ? (
+          <div>
+            <div key={bar.id} className='max-w-screen-xl px-4 mx-auto md:px-8'>
+              <div className='mb-10 md:mb-16'>
+                <h1 className='mb-4 text-6xl font-thin text-center uppercase text-primary md:mb-6 lg:text-7xl'>
+                  {bar.name}
+                </h1>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Loading />
+        )}
+        <div>
+          {bar.cocktails.map((eachDrink) => {
+            <div className='card w-96 bg-neutral text-neutral-content'>
+              <div className='items-center text-center card-body'>
+                <h2 className='card-title'>Cookies!</h2>
+                <p>We are using cookies for no reason.</p>
+                <div className='justify-end card-actions'>
+                  <button className='btn btn-primary'>Accept</button>
+                  <button className='btn btn-ghost'>Deny</button>
+                </div>
+              </div>
+            </div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
