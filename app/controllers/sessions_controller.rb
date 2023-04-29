6cc7f2a9aed37a936560c29161ac9c49fea0c18a
majaliju <majalijunyc@gmail.com>
2023-04-29 14:58:50 -0400

    barcocktail custom methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,8 @@
 class SessionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :user_doesnt_exist
 
-#####! these are copied from my old project
-#####~ I will tighten these up and refine them
-#####^ move foundational methods/validation to ApplicationController
 
-# authenticate the user; set session[:user_id] to their user.id
-# also set the session[:ip_address] to theirs
-# also set user[:ip_address] to theirs as well
+
 def create
   user = User.find_by!(username: params[:username])
   if user&.authenticate(params[:password])
