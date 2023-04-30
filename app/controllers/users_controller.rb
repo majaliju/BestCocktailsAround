2096b18d3f06d75b6062612f3d60518bc20e56ef
majaliju <majalijunyc@gmail.com>
2023-04-29 20:56:41 -0400

    user map works flawlessly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,86 +8,42 @@ class UsersController < ApplicationController
     render json: user, status: :created
   end
 
-    ## an update method that updates the address and returns lat, longitude
-    def update
+  ## an update method that updates the address and returns lat, longitude
+  def update
       # this is the geolocation version
       user = User.find_by!(id: session[:user_id]) 
       street = params[:street]
       city = params[:city]
       state = params[:state]
       country = params[:country]
+      postcode = params[:postcode]
+      address = params[:address]
       full_address = [street, city, state, country].compact.join(', ')
-      results = Geocoder.search(full_address)
-      #  save these results also to the database so they persist
 
-      # for whatever reason, password validation is checked here which isn't essential
-      user.update(
-        latitude: results.first.coordinates[0],
-        longitude: results.first.coordinates[1],
-        address: full_address
-      )
-      user.save(:validate => false)
+      results = Geocoder.search(full_address)
+    if results.empty?
+      postcode_search = Geocoder.search(postcode)
+      # binding.break
+      if !postcode_search.empty?
+        lat_long = postcode_search.first.coordinates
+      end
+      else
+        lat_long = results.first.coordinates
+      end
+      if lat_long && !lat_long.empty?
+        user.update(
+          latitude: lat_long[0],
+          longitude: lat_long[1],
+          address: full_address
+        )
+        user.save(:validate => false)
+      end
+      
       render json: user, status: 200
     end
 
-  # def get_address
-  #   user = User.find_by(id: session[:user_id])
-  #   user[:address] = params[:address]
-  # # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-    
-  # end
-
-  # ## a show method that returns the user matching session[:user_id]
-  # def show
-  #   user = User.find_by!(id: session[:user_id])
-  #   render json: user, status: 200
-  # end
 
 
-
-
-  # # GET /users
-  # def index
-  #   @users = User.all
-
-  #   render json: @users
-  # end
-
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
-
-  # # POST /users
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     render json: @user, status: :created, location: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /users/1
-  # def destroy
-  #   @user.destroy
-  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -100,3 +56,97 @@ class UsersController < ApplicationController
       params.permit(:username, :password, :password_confirmation, :latitude, :longitude, :address, :ip_address)
     end
 end
+
+  # ## a working version for all address but doesnt save the user
+  # def update
+  #   # this is the geolocation version
+  #   user = User.find_by!(id: session[:user_id]) 
+  #   street = params[:street]
+  #   city = params[:city]
+  #   state = params[:state]
+  #   country = params[:country]
+  #   postcode = params[:postcode]
+  #   address = params[:address]
+  #   full_address = [street, city, state, country].compact.join(', ')
+
+  #   results = Geocoder.search(full_address)
+  # if results.empty?
+  #   postcode_search = Geocoder.search(postcode)
+  #   # binding.break
+  #   if !postcode_search.empty?
+  #     lat_long = postcode_search.first.coordinates
+  #   end
+  #   else
+  #     lat_long = results.first.coordinates
+  #   end
+  #   if lat_long && !lat_long.empty?
+  #     user.latitude = lat_long[0]
+  #     user.longitude = lat_long[1]
+  #   end
+    
+  #   render json: user, status: 200
+  # end
+
+
+
+
+    # ## an update method that updates the address and returns lat, longitude
+    # def update
+    #   # this is the geolocation version
+    #   user = User.find_by!(id: session[:user_id]) 
+    #   street = params[:street]
+    #   city = params[:city]
+    #   state = params[:state]
+    #   country = params[:country]
+    #   address = [street, city, state, country].compact.join(', ')
+    #   # results = Geocoder.search(street)
+    #   # binding.break
+    #   # user.update(
+    #   #   latitude: results.lat,
+    #   #   longitude: results.lon,
+    #   #   address: full_address
+    #   # )
+    #   #  save these results also to the database so they persist
+    #   # binding.break
+    #   # ! if results are nil then set an error condition to choose a different address
+    #   # for whatever reason, password validation is checked here which isn't essential
+    #   results = Geocoder.search(address)
+    #   if (results.coordinates[0] === nil){
+    #     puts "nil, try a new address!"
+    #   }
+    #   elsif (results.coordinates.exist?){
+    #     user.update(
+    #       latitude: results.first.coordinates[0],
+    #       longitude: results.first.coordinates[1],
+    #       address: address
+    #     )
+    #     user.save(:validate => false)
+    # }
+    #   end
+    #   render json: user, status: 200
+    # end
+
+
+
+    #^^ the original method
+        # ## an update method that updates the address and returns lat, longitude
+        # def update
+        #   # this is the geolocation version
+        #   user = User.find_by!(id: session[:user_id]) 
+        #   street = params[:street]
+        #   city = params[:city]
+        #   state = params[:state]
+        #   country = params[:country]
+        #   full_address = [street, city, state, country].compact.join(', ')
+        #   results = Geocoder.search(full_address)
+        #   #  save these results also to the database so they persist
+    
+        #   # for whatever reason, password validation is checked here which isn't essential
+        #   user.update(
+        #     latitude: results.first.coordinates[0],
+        #     longitude: results.first.coordinates[1],
+        #     address: full_address
+        #   )
+        #   user.save(:validate => false)
+        #   render json: user, status: 200
+        # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,13 @@ class User < ApplicationRecord
   
   # geocoded_by :address
   # after_validation :geocode
-## user's address is geocoded; we get lat-lng upon their address entering
+  #   ## user's address is geocoded; we get lat-lng upon their address entering
 
   
   has_many :reviews
   has_many :bar_cocktails, through: :reviews
 
-  validates :username, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8, maximum: 254 }
+
 
   # def address
   #   [street, city, state, country].compact.join(', ')
